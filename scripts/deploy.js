0cdf2732ab69d22ab1d9155c0b76b6209eb0e7dd
faberine <yashaswi.aryan99@gmail.com>
2023-06-10 17:49:32 +0530

    Created a custom Hello World contract and deployed on etherium testnet
    Created HelloWorld.sol and used it in deploy.js. Added etherium network
    node url for processing and validating the contract. Added personal wallet
    private key for trns and etherscan key for verification. Added code in
    deploy.js for verification on etherscan.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,23 +6,40 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
   const lockedAmount = hre.ethers.parseEther("0.001");
 
+  const contract = await hre.ethers.deployContract("HelloWorld");
+  /*
   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
     value: lockedAmount,
   });
+  */
 
-  await lock.waitForDeployment();
+  //await lock.waitForDeployment();
+  await contract.waitForDeployment();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+
+  console.log(`Contract deployed to ${contract.target}`);
+
+  //Delay
+  await sleep(15 * 1000);
+
+  await hre.run("verify:verify", {
+    address: contract.target,
+    constructorArgumnets: [],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
